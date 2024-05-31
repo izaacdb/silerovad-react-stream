65a6771a29dd96677b58fbc212bbb1fee56b0128
Izaac Broadbent <izaac.broadbent@gmail.com>
2024-05-31 13:22:12 +0100

    Fixed microphone activation / deactivation

--- test-site/src/react-basic/index.jsx
+++ test-site/src/react-basic/index.jsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
 
-import React, { useReducer, useState } from "react"
+import React, { useEffect, useReducer, useState } from "react"
 import * as ort from "onnxruntime-web"
 import { createRoot } from "react-dom/client"
 import { useMicVAD, utils } from "@ricky0123/vad-react"
@@ -9,7 +9,7 @@ ort.env.wasm.wasmPaths = {
   "ort-wasm-simd-threaded.wasm": "/ort-wasm-simd-threaded.wasm",
   "ort-wasm-simd.wasm": "/ort-wasm-simd.wasm",
   "ort-wasm.wasm": "/ort-wasm.wasm",
-  "ort-wasm-threaded.wasm": "/ort-wasm-threaded.wasm",
+  "ort-wasm-threaded.wasm": "/ort-wasm-threaded.wasm"
 }
 
 const domContainer = document.querySelector("#root")
@@ -21,6 +21,8 @@ function App() {
   const vad = useMicVAD({
     workletURL: "http://localhost:8080/vad.worklet.bundle.min.js",
     modelURL: "http://localhost:8080/silero_vad.onnx",
+    startOnReady: false,
+    initOnLoad: false,
     onVADMisfire: () => {
       console.log("Vad misfire")
     },
@@ -33,25 +35,55 @@ function App() {
       const base64 = utils.arrayBufferToBase64(wavBuffer)
       const url = `data:audio/wav;base64,${base64}`
       setAudioList((old) => [url, ...old])
-    },
+    }
   })
+
+
   return (
     <section className="section">
       <div className="container">
-        <h1 className="title">Basic vad-react functionality</h1>
+        <h1 className="title">Microphone fix fork of vad-react</h1>
 
-        <div className="block is-inline-flex">
+        <div className="block ">
+          <div>state: {JSON.stringify({ "vad.ready": vad.ready ? 1 : 0, "vad.listening": vad.listening ? 1 : 0 })}</div>
           <button
+            style={{marginRight:'1rem'}}
             className={
               vad.loading ? "button is-primary is-loading" : "button is-primary"
             }
             onClick={() => {
-              console.log("run toggle vad")
-              vad.toggle()
+              console.log("initialize")
+              vad.initialize()
             }}
-            disabled={vad.loading}
+            disabled={vad.ready}
           >
-            Toggle VAD
+            Start microphone
+          </button>
+          <button
+            style={{marginRight:'1rem'}}
+            className={
+              vad.loading ? "button is-primary is-loading" : "button is-primary"
+            }
+            onClick={() => {
+              console.log("vad.start")
+              vad.start()
+            }}
+            disabled={!vad.ready || vad.listening}
+          >
+            Start VAD
+          </button>
+          <button
+            style={{marginRight:'1rem'}}
+            className={
+              vad.loading ? "button is-primary is-loading" : "button is-primary"
+            }
+            onClick={() => {
+              console.log("vad.terminate")
+              vad.terminate()
+            }}
+            disabled={!vad.ready || !vad.listening}
+          >
+            Stop microphone & vad
           </button>
         </div>
 
